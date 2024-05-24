9fdaf02517890677bafaab3dc6de4f4692fac688
asadbekdev07 <futureprotrader1007@gmail.com>
2024-05-24 22:29:43 +0500

    Optimize slider css and react codes

--- src/assets/components/Slider-hero.jsx
+++ src/assets/components/Slider-hero.jsx
@@ -1,227 +1,112 @@
-import React, { useRef, useState } from 'react';
-// Import Swiper React components
+import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-// IMAGES
-import istanbulImg from '../../../public/img/istanbul.jpg'
-import antalyaImg from '../../../public/img/antalya.jpg'
-import dubaiImg from '../../../public/img/dubai2.jpg'
-import sharmImg from '../../../public/img/sharm.jpg'
-
-// import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import istanbulImg from '../../../public/img/istanbul.jpg';
+import antalyaImg from '../../../public/img/antalya.jpg';
+import dubaiImg from '../../../public/img/dubai2.jpg';
+import sharmImg from '../../../public/img/sharm.jpg';
+
+const slidesData = [
+  {
+    img: istanbulImg,
+    city: 'Istanbul',
+    population: '15.5 M',
+    territory: '5.461 km²',
+    avgPrice: '$946.000'
+  },
+  {
+    img: antalyaImg,
+    city: 'Antalya',
+    population: '1.5 M',
+    territory: '1.417 km²',
+    avgPrice: '$1.100.200'
+  },
+  {
+    img: dubaiImg,
+    city: 'Dubai',
+    population: '3.3 M',
+    territory: '35 km²',
+    avgPrice: '$425.000'
+  },
+  {
+    img: sharmImg,
+    city: 'Sharm El-Sheikh',
+    population: '0.07 M',
+    territory: '45 km²',
+    avgPrice: '$165.450'
+  }
+];
+
+const SlideContent = ({ img, city, population, territory, avgPrice }) => (
+  <section className="slider" style={{ backgroundImage: `url(${img})` }}>
+    <div className="container slider__container">
+      <div className="slider__top">
+        <h3 className="slider__top-heading">Take a Glimpse Into The Beautiful City Of:</h3>
+        <h2 className="slider__top-city">{city}</h2>
+        <a className="button button--cta" href="#contact">Go There</a>
+      </div>
+      <div className="slider__bottom">
+        <ul className="slider__bottom-list list">
+          <li className="list__item">
+            <div className="list__item-inner">
+              <i className="fa fa-user"></i>
+              <h4 className="list__subheading">
+                <span>Population:</span><br />{population}
+              </h4>
+            </div>
+          </li>
+          <li className="list__item">
+            <div className="list__item-inner">
+              <i className="fa fa-globe"></i>
+              <h4 className="list__subheading">
+                <span>Territory:</span><br />{territory}
+              </h4>
+            </div>
+          </li>
+          <li className="list__item">
+            <div className="list__item-inner">
+              <i className="fa fa-home"></i>
+              <h4 className="list__subheading">
+                <span>Avg Price:</span><br />{avgPrice}
+              </h4>
+            </div>
+          </li>
+          <div className="list__item-inner">
+            <a className="button" href="#contact">Explore More</a>
+          </div>
+        </ul>
+      </div>
+    </div>
+  </section>
+);
 
 export default function SliderHero() {
   return (
-    <>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 5500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={false}
-        modules={[Autoplay, Pagination, Navigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <section className="slider" style={{backgroundImage: `url(${istanbulImg})`}}>
-            <div className="container slider__container">
-            <div className="slider__top">
-              <h3 className="slider__top-heading">Take a Glimpse Into The Beautiful City Of:</h3>
-              <h2 className="slider__top-city">Istanbul</h2>
-              <a className="button button--cta" href="#contact">Go There</a>
-            </div>
-
-            <div className="slider__bottom">
-              <ul className="slider__bottom-list list">
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-user"></i>
-                    <h4 className="list__subheading">
-                      <span>Population:</span><br/>15.5 M
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-globe"></i>
-                    <h4 className="list__subheading">
-                      <span>Territory:</span><br/>5.461 km&#178;
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-home"></i>
-                    <h4 className="list__subheading">
-                      <span>Avg Price:</span><br/>$946.000
-                    </h4>
-                  </div>
-                </li>
-                <div className="list__item-inner">
-                  <a className="button" href="#contact">Explore More</a>
-                </div>
-
-              </ul>
-            </div>
-            </div>
-          </section>
+    <Swiper
+      spaceBetween={5}
+      centeredSlides={true}
+      autoplay={{
+        delay: 5500,
+        disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true,
+      }}
+      loop={true}
+      navigation={false}
+      modules={[Autoplay, Pagination, Navigation]}
+      className="mySwiper"
+    >
+      {slidesData.map((slide, index) => (
+        <SwiperSlide key={index}>
+          <SlideContent {...slide} />
         </SwiperSlide>
-        <SwiperSlide>
-        <section className="slider" style={{backgroundImage: `url(${antalyaImg})`}}>
-            <div className="container slider__container">
-            <div className="slider__top">
-              <h3 className="slider__top-heading">Take a Glimpse Into The Beautiful City Of:</h3>
-              <h2 className="slider__top-city">Antalya</h2>
-              <a className="button button--cta" href="#contact">Go There</a>
-            </div>
-
-            <div className="slider__bottom">
-              <ul className="slider__bottom-list list">
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-user"></i>
-                    <h4 className="list__subheading">
-                      <span>Population:</span><br/>1.5 M
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-globe"></i>
-                    <h4 className="list__subheading">
-                      <span>Territory:</span><br/>1.417 km&#178;
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-home"></i>
-                    <h4 className="list__subheading">
-                      <span>Avg Price:</span><br/>$1.100.200
-                    </h4>
-                  </div>
-                </li>
-                <div className="list__item-inner">
-                  <a className="button" href="#contact">Explore More</a>
-                </div>
-
-              </ul>
-            </div>
-            </div>
-          </section>
-        </SwiperSlide>
-        <SwiperSlide>
-        <section className="slider" style={{backgroundImage: `url(${dubaiImg})`}}>
-            <div className="container slider__container">
-            <div className="slider__top">
-              <h3 className="slider__top-heading">Take a Glimpse Into The Beautiful City Of:</h3>
-              <h2 className="slider__top-city">Dubai</h2>
-              <a className="button button--cta" href="#contact">Go There</a>
-            </div>
-
-            <div className="slider__bottom">
-              <ul className="slider__bottom-list list">
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-user"></i>
-                    <h4 className="list__subheading">
-                      <span>Population:</span><br/>3.3 M
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-globe"></i>
-                    <h4 className="list__subheading">
-                      <span>Territory:</span><br/>35 km&#178;
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-home"></i>
-                    <h4 className="list__subheading">
-                      <span>Avg Price:</span><br/>$425.000
-                    </h4>
-                  </div>
-                </li>
-                <div className="list__item-inner">
-                  <a className="button" href="#contact">Explore More</a>
-                </div>
-
-              </ul>
-            </div>
-            </div>
-          </section>
-        </SwiperSlide>
-        <SwiperSlide>
-        <section className="slider" style={{backgroundImage: `url(${sharmImg})`}}>
-            <div className="container slider__container">
-            <div className="slider__top">
-              <h3 className="slider__top-heading">Take a Glimpse Into The Beautiful City Of:</h3>
-              <h2 className="slider__top-city">Sharm El-Sheikh</h2>
-              <a className="button button--cta" href="#contact">Go There</a>
-            </div>
-
-            <div className="slider__bottom">
-              <ul className="slider__bottom-list list">
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-user"></i>
-                    <h4 className="list__subheading">
-                      <span>Population:</span><br/>0.07 M
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-globe"></i>
-                    <h4 className="list__subheading">
-                      <span>Territory:</span><br/>45 km&#178;
-                    </h4>
-                  </div>
-                </li>
-                <li className="list__item">
-
-                  <div className="list__item-inner">
-                    <i className="fa fa-home"></i>
-                    <h4 className="list__subheading">
-                      <span>Avg Price:</span><br/>$165.450
-                    </h4>
-                  </div>
-                </li>
-                <div className="list__item-inner">
-                  <a className="button" href="#contact">Explore More</a>
-                </div>
-
-              </ul>
-            </div>
-            </div>
-          </section>
-        </SwiperSlide>
-      </Swiper>
-    </>
+      ))}
+    </Swiper>
   );
 }
